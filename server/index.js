a122e9a41f15821153601552810514a077c860f4
Bethly9996 <bethlyc@kabarak.ac.ke>
2023-01-16 20:31:47 +0300

    added login route and used Axios fro HTTP requests

--- server/index.js
+++ server/index.js
@@ -1,8 +1,18 @@
 const mysql = require("mysql2");
+const cors = require("cors");
 const express = require("express");
 const app = express();
 
 app.use(express.json());
+
+//adding CORS
+app.use(
+    cors({
+        origin: ["http://localhost:3000"],
+        methods: ["GET", "POST"],
+        credentials: true,
+    })
+);
 app.listen(3001, () => {
     console.log("running server");
 });
@@ -11,7 +21,46 @@ const db = mysql.createConnection({
     user: "root",
     host: "localhost",
     email:"add-email",
+    registrationno: "add-registration number",
     password: "add-password",
     database: "loginsystem",
+});
+
+//login route
+app.post('/register', (req,res) => {
+
+    const username = req.body.username;
+    const email = req.body.email;
+    const registrationno = req.registrationno;
+    const password = req.password;
+
+    db.execute(
+        "INSERT INTO users (username,email,registration.no,password) VALUES (?,?,?,?",
+        [username, email, registrationno, password],
+        (err,result) => {
+            console.log(err);
+        }
+    );
+
+});
+
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    db.execute(
+        "SELECT * FROM users WHERE username = ? AND password = ?",
+        [username, password],
+        (err,result) => {
+            if (err) {
+                res.send({err:err});
+            }
+
+            if (result.length > 0) {
+                res.send( result);
+
+            } else({message:"Wrong username/password combination!"});
+        }
+    );
 });
 
--- src/App.js
+++ src/App.js
@@ -1,31 +1,84 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Axios from 'axios';
 
 const App = () => {
   const [usernameReg, setUsernameReg] = useState("");
   const [emailReg, setEmailReg] = useState("");
+  const [registrationnoReg, setRegistrationNo] =useState("");
   const [passwordReg, setPasswordReg] = useState("");
+
+  
+  const register = () => {
+    //Axios helps make HTTP request to HTTP servers
+    Axios.post("http://localhost:3001/register", {
+      username: usernameReg,
+      email: emailReg,
+      registrationno: registrationnoReg,
+      password: passwordReg,
+    }).then((response) => {
+      console.log(response);
+    });
+  };
+
+  //using state to get the username and password 
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loginStatus, setLoginStatus] = useState("");
+
+  //sending HTTP requests to "/login" endpoint using Axios
+
+  const login = () => {
+    Axios.post("http://localhost:3001/login", {
+      username: username,
+      password: password,
+    }).then((response) => {
+      if (!response.data.message) {
+        setLoginStatus(response.data.message);
+      } else {
+        setLoginStatus (response.data[0].message);
+      }
+    });
+      
+    };
+
   return (
     <div className="App">
       <div className='registration'>
         <h1>Registration</h1>
         <label>Username</label>
-        <input type="text"/><br/>
+        <input type="text" onChange={(e) => {
+          setUsernameReg(e.target.value);
+        }}/><br/>
         <label>Email</label>
-        <input type="text"/><br/>
+        <input type="text" onChange={(e) => {
+          setEmailReg(e.target.value);
+        }}/><br/>
         <label>Registrtation.No</label>
-        <input type="text"/><br/>
+        <input type="text" onChange={(e) => {
+          setRegistrationNo(e.target.value);
+        }}/><br/>
         <label>Password</label>
-        <input type="password"/> <br/>
-        <button>Register</button>
+        <input type="password" onChange={(e) => {
+          setPasswordReg(e.target.value);
+        }}/> <br/>
+
+        {/* using onClick attribute to connect the click events to the function register making use of the Axios module to initiate the POST requests */}
+        <button onClick={register}>Register</button> 
 
       </div>
       <div className='login'>
         <h1>Login</h1>
-        <input type='text' placeholder='Username...'/><br/>
-        <input type="password" placeholder='Password...'/>
-        <button >Login</button>
+        <input type='text' placeholder='Username...' onChange={ (e) => {
+          setUsername (e.target.value);
+        }}/><br/>
+        <input type="password" placeholder='Password...' onChange={(e) => {
+          setPassword (e.target.value);
+        }}/>
+        <button onClick={login} >Login</button>
       </div>
+      <h1> {loginStatus}</h1>
     </div>
   );
 }
