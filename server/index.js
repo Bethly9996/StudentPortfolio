6c5c5d4bfff28c8126d337e669d90c9b18361704
Bethly9996 <bethlyc@kabarak.ac.ke>
2023-01-17 12:10:07 +0300

    hashing passwords using bcrypt

--- server/index.js
+++ server/index.js
@@ -1,66 +1,83 @@
 const mysql = require("mysql2");
 const cors = require("cors");
+const bcrypt = require("bcrypt");
 const express = require("express");
+const { response } = require("express");
 const app = express();
 
 app.use(express.json());
 
 //adding CORS
 app.use(
-    cors({
-        origin: ["http://localhost:3000"],
-        methods: ["GET", "POST"],
-        credentials: true,
-    })
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
 );
 app.listen(3001, () => {
-    console.log("running server");
+  console.log("running server");
 });
 
 const db = mysql.createConnection({
-    user: "root",
-    host: "localhost",
-    email:"add-email",
-    registrationno: "add-registration number",
-    password: "add-password",
-    database: "loginsystem",
+  user: "root",
+  host: "localhost",
+  email: "add-email",
+  registrationno: "add-registration number",
+  password: "add-password",
+  database: "loginsystem",
 });
+
+//saltRound defines the no of hashing rounds done during bcrypt hashing
+
+const saltRound = 10;
 
 //login route
-app.post('/register', (req,res) => {
+app.post("/register", (req, res) => {
+  const username = req.body.username;
+  const email = req.body.email;
+  const registrationno = req.registrationno;
+  const password = req.password;
 
-    const username = req.body.username;
-    const email = req.body.email;
-    const registrationno = req.registrationno;
-    const password = req.password;
+  bcrypt.hash(password, saltRound, (err, hash) => {
+    if (err) {
+      console.log(err);
+    }
 
     db.execute(
-        "INSERT INTO users (username,email,registration.no,password) VALUES (?,?,?,?",
-        [username, email, registrationno, password],
-        (err,result) => {
-            console.log(err);
-        }
+      "INSERT INTO users (username,email,registration.no,password) VALUES (?,?,?,?",
+      [username, email, registrationno, password],
+      (err, result) => {
+        console.log(err);
+      }
     );
-
+  });
 });
 
-app.post('/login', (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
 
-    db.execute(
-        "SELECT * FROM users WHERE username = ? AND password = ?",
-        [username, password],
-        (err,result) => {
-            if (err) {
-                res.send({err:err});
+  db.execute(
+    "SELECT * FROM users WHERE username = ?;",
+    [username],
+    (err, result) => {
+      if (err) {
+        res.send({ err: err });
+      }
+
+      if (result.length > 0) {
+        bcrypt.compare(password, result[0].password,(error, response) => {
+            if (response) {
+                res.send(result);
+            } else {
+                res.send({message: "Wrong username/ password combination!"});
             }
-
-            if (result.length > 0) {
-                res.send( result);
-
-            } else({message:"Wrong username/password combination!"});
-        }
-    );
+        });
+      } else {
+        res.send({message:"User doesn't exist"});
+      }
+      }
+    
+  );
 });
-
--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import React, { useState } from 'react';
-import './App.css';
-import Axios from 'axios';
+import React, { useState } from "react";
+import "./App.css";
+import Axios from "axios";
 
 const App = () => {
   const [usernameReg, setUsernameReg] = useState("");
   const [emailReg, setEmailReg] = useState("");
-  const [registrationnoReg, setRegistrationNo] =useState("");
+  const [registrationnoReg, setRegistrationNo] = useState("");
   const [passwordReg, setPasswordReg] = useState("");
 
-  
   const register = () => {
     //Axios helps make HTTP request to HTTP servers
     Axios.post("http://localhost:3001/register", {
@@ -21,7 +20,7 @@ const App = () => {
     });
   };
 
-  //using state to get the username and password 
+  //using state to get the username and password
 
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -37,50 +36,72 @@ const App = () => {
       if (!response.data.message) {
         setLoginStatus(response.data.message);
       } else {
-        setLoginStatus (response.data[0].message);
+        setLoginStatus(response.data[0].message);
       }
     });
-      
-    };
+  };
 
   return (
     <div className="App">
-      <div className='registration'>
+      <div className="registration">
         <h1>Registration</h1>
         <label>Username</label>
-        <input type="text" onChange={(e) => {
-          setUsernameReg(e.target.value);
-        }}/><br/>
+        <input
+          type="text"
+          onChange={(e) => {
+            setUsernameReg(e.target.value);
+          }}
+        />
+        <br />
         <label>Email</label>
-        <input type="text" onChange={(e) => {
-          setEmailReg(e.target.value);
-        }}/><br/>
+        <input
+          type="text"
+          onChange={(e) => {
+            setEmailReg(e.target.value);
+          }}
+        />
+        <br />
         <label>Registrtation.No</label>
-        <input type="text" onChange={(e) => {
-          setRegistrationNo(e.target.value);
-        }}/><br/>
+        <input
+          type="text"
+          onChange={(e) => {
+            setRegistrationNo(e.target.value);
+          }}
+        />
+        <br />
         <label>Password</label>
-        <input type="password" onChange={(e) => {
-          setPasswordReg(e.target.value);
-        }}/> <br/>
-
+        <input
+          type="password"
+          onChange={(e) => {
+            setPasswordReg(e.target.value);
+          }}
+        />{" "}
+        <br />
         {/* using onClick attribute to connect the click events to the function register making use of the Axios module to initiate the POST requests */}
-        <button onClick={register}>Register</button> 
-
+        <button onClick={register}>Register</button>
       </div>
-      <div className='login'>
+      <div className="login">
         <h1>Login</h1>
-        <input type='text' placeholder='Username...' onChange={ (e) => {
-          setUsername (e.target.value);
-        }}/><br/>
-        <input type="password" placeholder='Password...' onChange={(e) => {
-          setPassword (e.target.value);
-        }}/>
-        <button onClick={login} >Login</button>
+        <input
+          type="text"
+          placeholder="Username..."
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        />
+        <br />
+        <input
+          type="password"
+          placeholder="Password..."
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <button onClick={login}>Login</button>
       </div>
       <h1> {loginStatus}</h1>
     </div>
   );
-}
+};
 
 export default App;
